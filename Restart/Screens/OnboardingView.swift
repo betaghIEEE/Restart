f792e37792e61211a7848081d1901c07e164903c
danielbeatty <dan.beatty@me.com>
2021-11-05 18:41:13 -0700

    Starting the footer section of the Onboarding view.

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -18,14 +18,50 @@ struct OnboardingView: View {
                 .ignoresSafeArea(.all, edges: .all)
             
             VStack(spacing: 20) {
-                Text("Onboarding")
-                    .font(.largeTitle)
+                // MARK:    - HEADER
                 
-                Button(action: {
-                    isOnboardingViewActive = false
-                }) {
-                    Text("Start")
-                }
+                Spacer()
+                VStack(spacing: 0){
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    Text("""
+                    It's not how much we give but
+                    how much love we put into giving.
+                    """)
+                        .font(.title3)
+                        .fontWeight(.light)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 10)
+                }   //: HEADER End
+                
+                // MARK:    - CENTER
+                
+                ZStack{
+                    ZStack{
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                       
+                    } //: Inner ZStack
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                } //: CENTER
+                Spacer()
+                
+                
+                // MARK:    - FOOTER
+                
+                ZStack{
+                    
+                } //: FOOTER end
+                
                 
             } //: VSTACK END
         }   //: ZSTACK
